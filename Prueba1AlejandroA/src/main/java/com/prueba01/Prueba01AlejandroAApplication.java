04e8857c9c6bc769697795738a981e2e7457fa5f
Alejandro-A22 <aacuna20386@ufide.ac.cr>
2024-10-30 18:42:04 -0600

    Creacion del proyecto

---
+++ Prueba1AlejandroA/src/main/java/com/prueba01/Prueba01AlejandroAApplication.java
@@ -0,0 +1,13 @@
+package com.prueba01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba01AlejandroAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba01AlejandroAApplication.class, args);
+	}
+
+}
---
+++ Prueba1AlejandroA/src/test/java/com/prueba01/Prueba01AlejandroAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba01AlejandroAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
